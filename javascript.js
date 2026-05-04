95c7332c40b6f1da3a94b23915be31518373cbbe
matias <rguilherme720@gmail.com>
2026-05-03 22:04:01 -0300

    Adicionando o detalhamento das habilidades e usando CSS para estilizar

--- javascript.js
+++ javascript.js
@@ -4,3 +4,15 @@ const menu = document.querySelector('.menu');
 toggle.addEventListener('click', () => {
     menu.classList.toggle('active');
 });
+const headers = document.querySelectorAll('.accordion-header');
+
+headers.forEach(header => {
+    header.addEventListener('click', () => {
+        const item = header.parentElement;
+        item.classList.toggle('active');
+
+        header.textContent = item.classList.contains('active')
+            ? '▲ Detalhes das Habilidades'
+            : '▼ Detalhes das Habilidades';
+    });
+});
